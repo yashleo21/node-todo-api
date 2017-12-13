35f738e5f30efff680f4a3ee75ba90cb90467623
yashleo21 <yashhacker21@gmail.com>
2017-12-14 01:22:11 +0530

    Added GET /todos:id route

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -32,7 +32,24 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send({});
+  }
+
+  Todo.findById(id).then( (todo) => {
+    if (!todo){
+      return res.status(404).send({});
+    }
+
+    res.status(200).send({todo});
+
+  }).catch( (e) => {
+    res.status(400).send({});
+  });
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
