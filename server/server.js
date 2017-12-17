1c30737eab3a9fc8219d8ee952848f2e845f1746
yashleo21 <yashhacker21@gmail.com>
2017-12-17 17:40:00 +0530

    Added Patch/ Update functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {mongoose} = require('./db/mongoose');
+
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
@@ -67,6 +69,33 @@ app.delete('/todos/:id', (req, res) =>{
 
     res.status(200).send({todo});
   }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true }).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch( (e) => {
     res.status(400).send();
   });
 });
